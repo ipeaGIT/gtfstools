1ae38a838fc766cbb8e1422724d4d224c70be3b7
dhersz <dhersz@gmail.com>
2020-11-07 19:02:47 -0300

    tweaks to pass package check

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,19 @@
 #' @importFrom dplyr %>%
 NULL
+
+utils::globalVariables(
+  c(
+    "stop_sequence",
+    "departure_time_hms",
+    "arrival_time_hms",
+    "last_stop_departure",
+    "segment",
+    "duration",
+    "result",
+    "geometry",
+    "shape_id",
+    "shape_pt_sequence",
+    "stop_sequence",
+    "origin_file"
+  )
+)
