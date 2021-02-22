dd90f31cd03e0c5c60c4d6a08372dea426a89a81
Daniel Herszenhut <dhersz@gmail.com>
2021-02-22 13:43:36 -0300

    final adjustments to v0.1.0

---
+++ R/gtfstools.R
@@ -0,0 +1,50 @@
+#' gtfstools: General Transit Feed Specification (GTFS) Editing and Analysing
+#' Tools
+#'
+#' Utility functions to read, manipulate, analyse and write transit feeds in the
+#' General Transit Feed Specification (GTFS) data format.
+#'
+#' @section Basic usage:
+#' Please check the introductory vignette for basic usage:
+#' \itemize{
+#'   \item{Run \code{vignette("gtfstools")};}
+#'   \item{Alternatively, check it on
+#'     \href{https://ipeagit.github.io/gtfstools/articles/gtfstools.html}{
+#'     \code{{gtfstools}} website}.}
+#' }
+#'
+#' @docType package
+#' @name gtfstools
+#' @aliases gtfstools-package
+#'
+#' @importFrom data.table := .I .SD %chin%
+"_PACKAGE"
+
+utils::globalVariables(
+  c(
+    ".",
+    "stop_sequence",
+    "departure_time_secs",
+    "arrival_time_secs",
+    "last_stop_departure",
+    "segment",
+    "duration",
+    "result",
+    "geometry",
+    "shape_id",
+    "shape_pt_sequence",
+    "stop_sequence",
+    "origin_file",
+    "file_spec",
+    "file_provided_status",
+    "field_provided_status",
+    "field_spec",
+    "validation_details",
+    "i.stop_lat",
+    "i.stop_lon",
+    "arrival_time",
+    "departure_time",
+    "i.duration",
+    "speed"
+  )
+)
---
+++ cran-comments.md
@@ -0,0 +1,23 @@
+## Test environments
+
+- Local Ubuntu 20.04 installation (R 4.0.4)
+- GitHub Actions:
+  - Windows (release, oldrel)
+  - MacOS (release, oldrel)
+  - Ubuntu 20.04 (devel, release, oldrel)
+- win-builder (devel, release, oldrel)
+- r-hub:
+  - Windows Server 2008 R2 SP1, R-devel, 32/64 bit
+  - Ubuntu Linux 20.04.1 LTS, R-release, GCC
+
+## R CMD check results
+
+0 errors | 0 warnings | 1 note
+
+* checking CRAN incoming feasibility ... NOTE
+  Maintainer: ‘Daniel Herszenhut <dhersz@gmail.com>’
+  New submission
+  Possibly mis-spelled words in DESCRIPTION:
+    GTFS (3:44, 15:68)
+
+This is gtfstools first submission. GTFS is an acronym for General Transit Feed Specification. The full specification name is provided both in the package title and description.
