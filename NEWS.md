1be4e11131dfcb8c85d4a6cc9c607880bbb46247
Daniel Herszenhut <dhersz@gmail.com>
2021-03-04 17:12:01 -0300

    adds file/field existence testing functions

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,5 @@
 # gtfstools 0.1.0.xxxx (dev)
 
-# gtfstools 0.1.0 (2021-02-23)
+# gtfstools 0.1.0
 
 - Initial CRAN release.
--- R/utils.R
+++ R/utils.R
@@ -231,3 +231,67 @@ copy_gtfs_without_field <- function(gtfs, file, field) {
   return(gtfs_copy)
 
 }
+
+
+
+#' Test for field existence in a GTFS text file
+#'
+#' Test for the existence of a given field in a GTFS text file (represented by
+#' a column in a \code{data.table}).
+#'
+#' @param gtfs A GTFS object as created by \code{\link{read_gtfs}}.
+#' @param file File in which the field should exist.
+#' @param field Field to check existence of.
+#'
+#' @return \code{TRUE}, if the check is successful, and \code{FALSE} otherwise.
+#'
+#' @keywords internal
+#'
+#' @export
+test_field_exists <- function(gtfs, file, field) {
+
+  checkmate::assert_class(gtfs, "dt_gtfs")
+  checkmate::assert_string(file)
+  checkmate::assert_character(field)
+
+  # check if file is an index of gtfs
+
+  checkmate::assert(check_gtfs_file_exists(gtfs, file))
+
+  # check if "field" is a column in "file"
+
+  non_existent_field <- field[! field %chin% names(gtfs[[file]])]
+
+  if (identical(non_existent_field, character(0))) return(TRUE)
+
+  return(FALSE)
+
+}
+
+
+
+#' Test for GTFS text file existence
+#'
+#' Tests for the existence of a given file (as represented by a
+#' \code{data.table}) in a GTFS object.
+#'
+#' @param gtfs A GTFS object as created by \code{\link{read_gtfs}}.
+#' @param file File to test the existence of.
+#'
+#' @return \code{TRUE}, if the check is successful, and \code{FALSE} otherwise.
+#'
+#' @keywords internal
+#'
+#' @export
+test_file_exists <- function(gtfs, file) {
+
+  checkmate::assert_class(gtfs, "dt_gtfs")
+  checkmate::assert_character(file)
+
+  non_existent_file <- file[! file %chin% names(gtfs)]
+
+  if (identical(non_existent_file, character(0))) return(TRUE)
+
+  return(FALSE)
+
+}
