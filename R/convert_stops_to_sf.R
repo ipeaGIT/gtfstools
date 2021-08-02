922480fbed8851fcc842967cbe36d92b2a5d01c8
Daniel Herszenhut <dhersz@gmail.com>
2021-08-02 13:03:02 -0300

    adjustments to convert_stops_to_sf()

--- R/convert_stops_to_sf.R
+++ R/convert_stops_to_sf.R
@@ -1,13 +1,14 @@
-#' @title Convert GTFS stops to simple feature object
+#' Convert `stops` table to simple feature object
 #'
-#' @description
-#' Returns the `stops.txt` data from a GTFS feed as a \code{POINT sf} object.
+#' Converts the `stops` table to a `POINT sf` object.
 #'
-#' @param gtfs A GTFS object as created by \code{\link{read_gtfs}}.
-#' @param crs The coordinate reference system (CRS) represented as an EPSG code.
-#'            Defaults to 4326 (latlong WGS84).
+#' @param gtfs A GTFS object.
+#' @param stop_id A character vector including the `stop_id`s to be converted.
+#'   If `NULL` (the default), all stops are converted.
+#' @param crs The CRS of the resulting object, either as an EPSG code or as an
+#'   `crs` object. Defaults to 4326 (WGS 84).
 #'
-#' @return A \code{POINT sf} object.
+#' @return A `POINT sf` object.
 #'
 #' @examples
 #' # read gtfs
@@ -17,22 +18,70 @@
 #' stops_sf <- convert_stops_to_sf(gtfs)
 #' head(stops_sf)
 #'
+#' stops_sf <- convert_stops_to_sf(gtfs, stop_id = "18848")
+#' stops_sf
 #'
 #' @export
-convert_stops_to_sf <- function(gtfs, crs = 4326){
+convert_stops_to_sf <- function(gtfs, stop_id = NULL, crs = 4326) {
 
-  ## input checking
+  # input checking
+
   checkmate::assert_class(gtfs, "dt_gtfs")
-  checkmate::assert_data_table(gtfs$stops, .var.name= 'gtfs$stops')
+  checkmate::assert_character(stop_id, null.ok = TRUE)
   checkmate::assert(
     checkmate::check_numeric(crs),
     checkmate::check_class(crs, "crs"),
     combine = "or"
   )
 
-  ## fun
-  temp_stops_sf <- sfheaders::sf_point(gtfs$stops, x = "stop_lon", y = "stop_lat", keep = TRUE)
-  sf::st_crs(temp_stops_sf) <- crs
+  gtfsio::assert_fields_types(
+    gtfs,
+    "stops",
+    c("stop_id", "stop_lon", "stop_lat"),
+    c("character", "numeric", "numeric")
+  )
 
-  return(temp_stops_sf)
+  # select relevant stop_ids
+
+  if (!is.null(stop_id))
+    relevant_stops <- stop_id
+  else
+    relevant_stops <- unique(gtfs$stops$stop_id)
+
+  # raise warning/error if given 'stop_id's don't exist in 'stops'
+
+  if (!is.null(stop_id)) {
+
+    invalid_stop_id <- stop_id[! stop_id %chin% unique(gtfs$stops$stop_id)]
+
+    if (identical(invalid_stop_id, stop_id))
+      stop("'stops' doesn't contain any of the ids passed to 'stop_id'.")
+
+    if (!identical(invalid_stop_id, character(0)))
+      warning(
+        paste0(
+          "'stops' doesn't contain the following stop_id(s): "),
+        paste0("'", invalid_stop_id, "'", collapse = ", ")
+      )
+
+  }
+
+  # filter 'stops' table and create sf from it
+
+  stops_sf <- gtfs$stops[stop_id %chin% relevant_stops]
+  stops_sf <- sfheaders::sf_point(
+    stops_sf,
+    x = "stop_lon",
+    y = "stop_lat",
+    keep = TRUE
+  )
+  stops_sf <- sf::st_set_crs(stops_sf, 4326)
+
+  # transform crs from 4326 to the one passed to 'crs'
+
+  if (crs != 4326 && crs != sf::st_crs(4326))
+    stops_sf <- sf::st_transform(stops_sf, crs)
+
+  return(stops_sf)
+
 }
--- tests/testthat/test-convert_stops_to_sf.R
+++ tests/testthat/test-convert_stops_to_sf.R
@@ -1,4 +1,4 @@
-context("Get convert_stops_to_sf")
+context("Convert stops to sf")
 
 
 # setup -------------------------------------------------------------------
@@ -9,35 +9,75 @@ gtfs <- read_gtfs(data_path)
 
 # tests -------------------------------------------------------------------
 
-### expected behavior
-test_that("convert_stops_to_sf - expected behavior", {
+test_that("raises errors due to incorrect input types", {
+  no_class_gtfs <- unclass(gtfs)
 
-  stops1 <- convert_stops_to_sf(gtfs)
+  expect_error(convert_stops_to_sf(no_class_gtfs))
+  expect_error(convert_stops_to_sf(gtfs, stop_id = as.factor("18848")))
+  expect_error(convert_stops_to_sf(gtfs, crs = "4326"))
 
-  # check class
-  expect_s3_class(convert_stops_to_sf(gtfs), 'sf')
+  wrong_types_gtfs <- read_gtfs(data_path)
+  wrong_types_gtfs$stops[, stop_id := as.factor(stop_id)]
+  expect_error(convert_stops_to_sf(wrong_types_gtfs))
 
-  # check projection
-  expect_equal(sf::st_crs(stops1)$input, "EPSG:4326")
+  wrong_types_gtfs$stops[
+    ,
+    `:=`(
+      stop_id = as.character(stop_id),
+      stop_lon = as.character(stop_lon)
+    )
+  ]
+  expect_error(convert_stops_to_sf(wrong_types_gtfs))
 
-  # check dimensions
-  expect_equal(nrow(stops1), 654)
-  expect_equal(ncol(stops1), 4)
-
+  wrong_types_gtfs$stops[
+    ,
+    `:=`(
+      stop_lon = as.numeric(stop_lon),
+      stop_lat = as.character(stop_lat)
+    )
+  ]
+  expect_error(convert_stops_to_sf(wrong_types_gtfs))
 })
 
-### expected errors and messages
+test_that("convert correct stops", {
+  # if 'stop_id' is NULL (default), all stops are converted
+  stops_sf <- convert_stops_to_sf(gtfs)
+  expect_identical(stops_sf$stop_id, gtfs$stops$stop_id)
 
-test_that("convert_stops_to_sf - errors and warnings", {
+  # else only (valid) stops are converted
+  stop_ids <- c("18848", "ola")
+  suppressWarnings(stops_sf <- convert_stops_to_sf(gtfs, stop_id = stop_ids))
+  expect_identical(stops_sf$stop_id, "18848")
+})
 
-  # Wrong crs
-  testthat::expect_error(convert_stops_to_sf(gtfs = gtfs, crs = 'aaaaa'))
+test_that("raises warnings/erros if invalid stop_ids are passed", {
+  # raises warning if a invalid stop_id is passed
+  expect_warning(convert_stops_to_sf(gtfs, stop_id = c("18848", "ola")))
 
-  # Wrong gtfs input
-  testthat::expect_error(convert_stops_to_sf(gtfs = 'aaaaa'))
+  # throws error if all passed stop_ids are invalid
+  expect_error(convert_stops_to_sf(gtfs, stop_id = "ola"))
+})
 
-  # No stops data
-  gtfs$stops <- NULL
-  testthat::expect_error(convert_stops_to_sf(gtfs = gtfs))
+test_that("returns a POINT sf with correct crs", {
+  # outputs a POINT sf
+  stops_sf <- convert_stops_to_sf(gtfs)
+  expect_s3_class(stops_sf, "sf")
+  expect_s3_class(stops_sf$geometry, "sfc_POINT")
 
+  # crs is 4326 by default
+  expect_identical(sf::st_crs(stops_sf), sf::st_crs(4326))
+
+  # which can be changed with 'crs' argument
+  stops_sf <- convert_stops_to_sf(gtfs, crs = 4674)
+  expect_identical(sf::st_crs(stops_sf), sf::st_crs(4674))
+})
+
+test_that("doesn't change passed gtfs object (only the index of gtfs$stops)", {
+  original_gtfs <- read_gtfs(data_path)
+  gtfs <- read_gtfs(data_path)
+  stops_sf <- convert_stops_to_sf(gtfs)
+
+  # 'stop_id' is set as gtfs$stops index because of data.table subset
+  data.table::setindex(gtfs$stops, NULL)
+  expect_identical(original_gtfs, gtfs)
 })
