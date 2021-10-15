38a5f66f2d39a5b2eebc79fb9b6164e1f00169fd
Daniel Herszenhut <dhersz@gmail.com>
2021-10-15 14:38:08 -0300

    more thorough convert_shapes_to_sf() checks

--- tests/testthat/test-convert_shapes_to_sf.R
+++ tests/testthat/test-convert_shapes_to_sf.R
@@ -1,4 +1,4 @@
-context("Get convert_shapes_to_sf")
+context("Convert shapes to sf")
 
 
 # setup -------------------------------------------------------------------
@@ -9,35 +9,88 @@ gtfs <- read_gtfs(data_path)
 
 # tests -------------------------------------------------------------------
 
-### expected behavior
-test_that("convert_shapes_to_sf - expected behavior", {
+test_that("raises errors due to incorrect input types", {
+  no_class_gtfs <- unclass(gtfs)
 
-  shape_sf <- convert_shapes_to_sf(gtfs)
+  expect_error(convert_shapes_to_sf(no_class_gtfs))
+  expect_error(convert_shapes_to_sf(gtfs, shape_id = as.factor("17846")))
+  expect_error(convert_shapes_to_sf(gtfs, crs = "4326"))
 
-  # check class
-  expect_s3_class(convert_shapes_to_sf(gtfs), 'sf')
+  wrong_types_gtfs <- read_gtfs(data_path)
+  wrong_types_gtfs$shapes[, shape_id := as.factor(shape_id)]
+  expect_error(convert_shapes_to_sf(wrong_types_gtfs))
 
-  # check projection
-  expect_equal(sf::st_crs(shape_sf)$input, "EPSG:4326")
+  wrong_types_gtfs$shapes[
+    ,
+    `:=`(
+      shape_id = as.character(shape_id),
+      shape_pt_lon = as.character(shape_pt_lon)
+    )
+  ]
+  expect_error(convert_shapes_to_sf(wrong_types_gtfs))
 
-  # check dimensions
-  expect_equal(nrow(shape_sf), 36)
-  expect_equal(ncol(shape_sf), 2)
+  wrong_types_gtfs$shapes[
+    ,
+    `:=`(
+      shape_pt_lon = as.numeric(shape_pt_lon),
+      shape_pt_lat = as.character(shape_pt_lat)
+    )
+  ]
+  expect_error(convert_shapes_to_sf(wrong_types_gtfs))
 
+  wrong_types_gtfs$shapes[
+    ,
+    `:=`(
+      shape_pt_lat = as.numeric(shape_pt_lon),
+      shape_pt_sequence = as.character(shape_pt_sequence)
+    )
+  ]
+  expect_error(convert_shapes_to_sf(wrong_types_gtfs))
 })
 
-### expected errors and messages
+test_that("convert correct shapes", {
+  # if 'shape_id' is NULL (default), all shapes are converted
+  shapes_sf <- convert_shapes_to_sf(gtfs)
+  expect_true(
+    all(gtfs$shapes$shape_id %chin% shapes_sf$shape_id)
+  )
 
-test_that("convert_shapes_to_sf - errors and warnings", {
+  # else only (valid) shapes are converted
+  shape_ids <- c("17846", "ola")
+  suppressWarnings(
+    shapes_sf <- convert_shapes_to_sf(gtfs, shape_id = shape_ids)
+  )
+  expect_identical(shapes_sf$shape_id, "17846")
+})
 
-  # Wrong crs
-  testthat::expect_error(convert_shapes_to_sf(gtfs = gtfs, crs = 'aaaaa'))
+test_that("raises warnings/erros if invalid stop_ids are passed", {
+  # raises warning if a invalid stop_id is passed
+  expect_warning(convert_shapes_to_sf(gtfs, shape_id = c("17846", "ola")))
 
-  # Wrong gtfs input
-  testthat::expect_error(convert_shapes_to_sf(gtfs = 'aaaaa'))
+  # throws error if all passed stop_ids are invalid
+  expect_error(convert_shapes_to_sf(gtfs, shape_id = "ola"))
+})
 
-  # No stops data
-  gtfs$shapes <- NULL
-  testthat::expect_error(convert_shapes_to_sf(gtfs = gtfs))
+test_that("returns a LINESTRING sf with correct crs", {
+  # outputs a LINESTRING sf
+  shapes_sf <- convert_shapes_to_sf(gtfs)
+  expect_s3_class(shapes_sf, "sf")
+  expect_s3_class(shapes_sf$geometry, "sfc_LINESTRING")
 
+  # crs is 4326 by default
+  expect_identical(sf::st_crs(shapes_sf), sf::st_crs(4326))
+
+  # which can be changed with 'crs' argument
+  shapes_sf <- convert_shapes_to_sf(gtfs, crs = 4674)
+  expect_identical(sf::st_crs(shapes_sf), sf::st_crs(4674))
+})
+
+test_that("doesn't change passed gtfs object (only the index of gtfs$shapes)", {
+  original_gtfs <- read_gtfs(data_path)
+  gtfs <- read_gtfs(data_path)
+  shapes_sf <- convert_shapes_to_sf(gtfs)
+
+  # 'shape_id' is set as gtfs$shapes index because of data.table subset
+  data.table::setindex(gtfs$shapes, NULL)
+  expect_identical(original_gtfs, gtfs)
 })
