364460f68f2d4f7f0f88a296ca5f9c03a267d2ce
Daniel Herszenhut <dhersz@gmail.com>
2021-10-22 15:01:26 -0300

    adjusted filter_route_type() and tests
    this function now heavily depends on filter_route_id(). the tests have
    changed to reflect such dependency as well.

--- R/filter_route_type.R
+++ R/filter_route_type.R
@@ -1,98 +1,96 @@
-#' @title Filter GTFS data by route type (transport mode)
+#' Filter GTFS object by `route_type` (transport mode)
 #'
-#' @description
-#' Filter a GTFS data byits route type (transport mode), subsetting routes
-#' and trips. It also removes the unnecessary stop_times, shapes, frequencies
-#' (if exist in a feed), and stops accordingly.
+#' Filters a GTFS object by `route_type`s, keeping (or dropping) the entries
+#' that relate to such types in each file.
 #'
-#' @param gtfs A GTFS object as created by \code{\link{read_gtfs}}.
-#' @param route_types A vector of route types belonging to the routes of the
-#'                    GTFS data. Each transport mode is associated with a
-#'                    `route_type` number. See Details.
-#' @param keep Logical. Whether the passed `route_ids` should be kept (Defaults
-#'             to `TRUE`), or whether they should be dropped.
+#' @param gtfs A GTFS object.
+#' @param route_type An integer vector. The `route_type`s used to filter the
+#'   data.
+#' @param keep A logical. Whether the entries related to the passed
+#'   `route_type`s should be kept or dropped (defaults to `TRUE`, which keeps
+#'   the entries).
 #'
-#' @return Returns a GTFS object with filtered routes, trips, shapes, stops,
-#'         stop_times and frequencies.
+#' @return The GTFS object passed to the `gtfs` parameter, after the filtering
+#' process.
 #'
-#' @details
-#' Route types:
-#' |**`route_type`**|**Transport mode**| Note |
-#' |-----|-----|-----|
-#' |0  | Tram, Streetcar, Light rail | Any light rail or street level system within a metropolitan area |
-#' |1  | Subway, Metro | Any underground rail system within a metropolitan area |
-#' |2  | Rail | Used for intercity or long-distance travel |
-#' |3  | Bus | Used for short- and long-distance bus routes |
-#' |4  | Ferry | Used for short- and long-distance boat service |
-#' |5  | Cable tram | Used for street-level rail cars where the cable runs beneath the vehicle, e.g., cable car in San Francisco |
-#' |6  | Aerial lift, suspended cable car (e.g., gondola lift, aerial tramway) | Cable transport where cabins, cars, gondolas or open chairs are suspended by means of one or more cables |
-#' |7  | Funicular | Any rail system designed for steep inclines |
-#' |11 |  Trolleybus | Electric buses that draw power from overhead wires using poles |
-#' |12 |  Monorail | Railway in which the track consists of a single rail or a beam |
+#' @details # Route types
+#' Valid options are:
+#' - 0 - Tram, Streetcar, Light rail. Any light rail or street level system
+#' within a metropolitan area.
+#' - 1 - Subway, Metro. Any underground rail system within a metropolitan area.
+#' - 2 - Rail. Used for intercity or long-distance travel.
+#' - 3 - Bus. Used for short- and long-distance bus routes.
+#' - 4 - Ferry. Used for short- and long-distance boat service.
+#' - 5 - Cable tram. Used for street-level rail cars where the cable runs
+#' beneath the vehicle, e.g., cable car in San Francisco.
+#' - 6 - Aerial lift, suspended cable car (e.g., gondola lift, aerial tramway).
+#' Cable transport where cabins, cars, gondolas or open chairs are suspended by
+#' means of one or more cables.
+#' - 7 - Funicular. Any rail system designed for steep inclines.
+#' - 11 - Trolleybus. Electric buses that draw power from overhead wires using
+#' poles.
+#' - 12 - Monorail. Railway in which the track consists of a single rail or a
+#' beam.
+#'
+#' @family filter functions
 #'
 #' @examples
 #' # read gtfs
 #' data_path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
 #' gtfs <- read_gtfs(data_path)
 #'
-#'# filter: Keep selected route types
-#' subset1 <- filter_route_type(gtfs, route_types=c(2, 3), keep = TRUE)
+#' object.size(gtfs)
 #'
-#'# filter: Drop selected route types
-#' subset2 <- filter_route_type(gtfs, route_types=c(2, 3), keep = FALSE)
+#' # keeps entries related to passed route_ids
+#' smaller_gtfs <- filter_route_type(gtfs, route_type = 1)
+#' object.size(smaller_gtfs)
 #'
-#' @family filter functions
+#' # drops entries related to passed trip_ids
+#' smaller_gtfs <- filter_route_type(gtfs, route_type = 1, keep = FALSE)
+#' object.size(smaller_gtfs)
+#'
 #' @export
-filter_route_type <- function(gtfs, route_types, keep = TRUE) {
+filter_route_type <- function(gtfs, route_type, keep = TRUE) {
 
-  ## input checking
-  checkmate::assert_logical(keep)
+  # input checking
+
   checkmate::assert_class(gtfs, "dt_gtfs")
-  checkmate::assert_vector(route_types)
-  checkmate::assert_numeric(route_types)
-  checkmate::assert_data_table(gtfs$routes, .var.name= 'gtfs$routes')
-    ## I like this check below because it has a more informative error message
-    # if(is.null(gtfs$routes)) {stop("GTFS data does not have routes")}
+  checkmate::assert_integerish(route_type)
+  checkmate::assert_logical(keep, len = 1)
 
-  # chech route_types values
-  all_modes <- c(0,1,2,3,4,5,6,7,1,12)
-  if( !all(route_types %in% all_modes)){
-     stop(paste0("One of the route_types input does not exist. Values must be
-                 among the following:", paste(all_modes, collapse = ", ")))
-    }
+  possible_types <- c(0, 1, 2, 3, 4, 5, 6, 7, 11, 12)
+  checkmate::assert_names(
+    as.character(route_type),
+    subset.of = as.character(possible_types),
+    .var.name = "route_type"
+  )
 
+  # storing the current environment to filter using the values of 'trip_id'
 
+  env <- environment()
 
-  ## filter operator
-  `%ffilter%` <- `%in%`
-  if (keep==FALSE) { `%ffilter%` <- Negate(`%in%`) }
+  # select the 'route_id's that correspond to the given 'route_type's to filter
+  # those using filter_route_id()
 
-  ## start filtering gtfs
-  # routes
-  gtfs$routes <- subset(gtfs$routes, route_type %ffilter% route_types)
+  if (gtfsio::check_fields_exist(gtfs, "routes", c("route_id", "route_type"))) {
 
-  # trips
-  route_ids <- unique(gtfs$routes$route_id)
-  gtfs$trips <- subset(gtfs$trips, route_id %chin% route_ids)
+    gtfsio::assert_fields_types(
+      gtfs,
+      "routes",
+      c("route_id", "route_type"),
+      c("character", "integer")
+    )
 
-  # shapes
-  shape_ids <- unique(gtfs$trips$shape_id)
-  gtfs$shapes <- subset(gtfs$shapes, shape_id %chin% shape_ids)
+    route_id <- gtfs$routes[
+      route_type %in% get("route_type", envir = env)
+    ]$route_id
 
-  # stop_times
-  trip_ids <- unique(gtfs$trips$trip_id)
-  gtfs$stop_times <- subset(gtfs$stop_times, trip_id %chin% trip_ids)
+    gtfs <- filter_route_id(gtfs, route_id, keep)
 
-  # frequencies
-  if(!is.null(gtfs$frequencies)){
-    gtfs$frequencies <- subset(gtfs$frequencies, trip_id %chin% trip_ids)
-    }
-
-  # stops
-  stop_ids <- unique(gtfs$stop_times$stop_id)
-  gtfs$stops <- subset(gtfs$stops, stop_id %chin% stop_ids)
+  }
 
   return(gtfs)
+
 }
 
 
--- tests/testthat/test-filter_route_type.R
+++ tests/testthat/test-filter_route_type.R
@@ -1,47 +1,61 @@
-context("Get filter_route_type")
+context("Filter by route_type")
 
 
 # setup -------------------------------------------------------------------
 
-data_path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
-gtfs <- read_gtfs(data_path)
+
+path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
+gtfs <- read_gtfs(path)
+route_type <- 1
 
 
 # tests -------------------------------------------------------------------
-
-### expected behavior
-test_that("filter_route_type - expected behavior", {
-
-  # check dimensions
-  subset1 <- filter_route_type(gtfs, route_types=c(2, 3), keep=TRUE)
-  expect_equal(nrow(subset1$routes), 13)
-  expect_equal(nrow(subset1$trips), 24)
-  expect_equal(nrow(subset1$shapes), 9277)
-
-  # check dimensions
-  subset2 <- filter_route_type(gtfs, route_types=c(2, 3), keep=FALSE)
-  expect_equal(nrow(subset2$routes), 6)
-  expect_equal(nrow(subset2$trips), 12)
-  expect_equal(nrow(subset2$shapes), 3018)
-
-  })
-
-### expected errors and messages
-
-test_that("filter_route_type - errors and warnings", {
-
-  # Wrong keep input
-  testthat::expect_error(filter_route_type(gtfs = gtfs, route_types= "6450-51", keep= 99999))
-
-  # Wrong gtfs input
-  testthat::expect_error(filter_route_type(gtfs = 'abcdefg'))
-
-  # Wrong route_types input
-  testthat::expect_error(filter_route_type(gtfs = gtfs, route_types= 99999))
-
-  # No routes files
-  gtfs$routes <- NULL
-  testthat::expect_error(filter_route_type(gtfs = gtfs, route_types= 3))
+# the large majority of tests are already conducted inside filter_route_id(),
+# which filter_route_type() calls. only a few sanity checks are included here
 
 
+test_that("raises error due to incorrect input types", {
+  expect_error(filter_route_type(unclass(gtfs), route_type))
+  expect_error(filter_route_type(gtfs, factor(route_type)))
+  expect_error(filter_route_type(gtfs, 124))
+  expect_error(filter_route_type(gtfs, route_type, keep = "TRUE"))
+})
+
+test_that("results in a dt_gtfs object", {
+  # a dt_gtfs object is a list with "dt_gtfs" and "gtfs" classes
+  dt_gtfs_class <- c("dt_gtfs", "gtfs", "list")
+  smaller_gtfs <- filter_route_type(gtfs, route_type)
+  expect_s3_class(smaller_gtfs, dt_gtfs_class)
+  expect_type(smaller_gtfs, "list")
+
+  # all objects inside a dt_gtfs are data.tables
+  invisible(lapply(smaller_gtfs, expect_s3_class, "data.table"))
+})
+
+test_that("doesn't change given gtfs", {
+  # (except for some tables' indices)
+
+  original_gtfs <- read_gtfs(path)
+  gtfs <- read_gtfs(path)
+  expect_identical(original_gtfs, gtfs)
+
+  smaller_gtfs <- filter_route_type(gtfs, route_type)
+  expect_false(identical(original_gtfs, gtfs))
+
+  data.table::setindex(gtfs$agency, NULL)
+  data.table::setindex(gtfs$calendar, NULL)
+  data.table::setindex(gtfs$frequencies, NULL)
+  data.table::setindex(gtfs$routes, NULL)
+  data.table::setindex(gtfs$shapes, NULL)
+  data.table::setindex(gtfs$stop_times, NULL)
+  data.table::setindex(gtfs$stops, NULL)
+  expect_identical(original_gtfs, gtfs)
+})
+
+test_that("'route_type' and 'keep' arguments work correctly", {
+  smaller_gtfs_keeping <- filter_route_type(gtfs, route_type)
+  expect_true(any(smaller_gtfs_keeping$routes$route_type %in% route_type))
+
+  smaller_gtfs_not_keeping <- filter_route_type(gtfs, route_type, keep = FALSE)
+  expect_true(!any(smaller_gtfs_not_keeping$routes$route_type %in% route_type))
 })
