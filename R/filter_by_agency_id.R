279d069c102b15e58be1fe80e443f4d86c306d97
Daniel Herszenhut <dhersz@gmail.com>
2021-12-02 18:23:03 -0300

    new function filter_by_agency_id()

---
+++ R/filter_by_agency_id.R
@@ -0,0 +1,214 @@
+#' Filter GTFS object by `agency_id`
+#'
+#' Filters a GTFS object by `agency_id`s, keeping (or dropping) the relevant
+#' entries in each file.
+#'
+#' @param gtfs A GTFS object.
+#' @param agency_id A character vector. The `agency_id`s used to filter the
+#' data.
+#' @param keep A logical. Whether the entries related to the specified
+#' `agency_id`s should be kept or dropped (defaults to `TRUE`, which keeps the
+#' entries).
+#'
+#' @return The GTFS object passed to the `gtfs` parameter, after the filtering
+#' process.
+#'
+#' @family filtering functions
+#'
+#' @examples
+#' data_path <- system.file("extdata/ber_gtfs.zip", package = "gtfstools")
+#' gtfs <- read_gtfs(data_path)
+#' agency_id <- "92"
+#'
+#' object.size(gtfs)
+#'
+#' # keeps entries related to passed agency_id
+#' smaller_gtfs <- filter_by_agency_id(gtfs, agency_id)
+#' object.size(smaller_gtfs)
+#'
+#' # drops entries related to passed agency_id
+#' smaller_gtfs <- filter_by_agency_id(gtfs, agency_id, keep = FALSE)
+#' object.size(smaller_gtfs)
+#'
+#' @export
+filter_by_agency_id <- function(gtfs, agency_id, keep = TRUE) {
+  checkmate::assert_class(gtfs, "dt_gtfs")
+  checkmate::assert_character(agency_id)
+  checkmate::assert_logical(keep, len = 1)
+
+  # selecting the filter operator used to filter 'agency_id's based on 'keep'.
+  # storing the current environment to filter using the values of 'agency_id'
+
+  `%ffilter%` <- `%chin%`
+  if (!keep) `%ffilter%` <- Negate(`%chin%`)
+
+  env <- environment()
+
+  # 'agency', 'routes', 'fare_attributes' and 'attributions' can be filtered
+  # using 'agency_id' itself, so `%ffilter%` is used. the other files depend on
+  # relational associations with 'agency_id' that come from these 4 tables.
+
+  # 'agency', 'attributes' and 'fare_attributes' (agency_id)
+
+  if (gtfsio::check_field_exists(gtfs, "agency", "agency_id")) {
+    gtfsio::assert_field_class(gtfs, "agency", "agency_id", "character")
+    gtfs$agency <- gtfs$agency[
+      agency_id %ffilter% get("agency_id", envir = env)
+    ]
+  }
+
+  if (gtfsio::check_field_exists(gtfs, "attributions", "agency_id")) {
+    gtfsio::assert_field_class(gtfs, "attributions", "agency_id", "character")
+    gtfs$attributions <- gtfs$attributions[
+      agency_id %ffilter% get("agency_id", envir = env)
+    ]
+  }
+
+  if (gtfsio::check_field_exists(gtfs, "fare_attributes", "agency_id")) {
+    # 'fare_attributes' could be used to filter 'fare_rules' via fare_id. but
+    # that introduces some complexity, because agency_id is an optional field
+    # in both 'fare_attributes' and 'routes' if 'agency' contains only one
+    # agency, and 'routes' can also be used to filter 'fare_rules' (via
+    # route_id). i'm opting to filter 'fare_rules' via the 'routes' path only,
+    # afterall a fare only makes sense if it's associated to a route, but i can
+    # change this behaviour later if needed
+    gtfsio::assert_field_class(
+      gtfs,
+      "fare_attributes",
+      "agency_id",
+      "character"
+    )
+    gtfs$fare_attributes <- gtfs$fare_attributes[
+      agency_id %ffilter% get("agency_id", envir = env)
+    ]
+  }
+
+  # routes (agency_id)
+
+  if (gtfsio::check_field_exists(gtfs, "routes", "agency_id")) {
+    gtfsio::assert_field_class(gtfs, "routes", "agency_id", "character")
+    gtfs$routes <- gtfs$routes[
+      agency_id %ffilter% get("agency_id", envir = env)
+    ]
+
+    # 'routes' allows us to filter by 'route_id'
+    relevant_routes <- unique(gtfs$routes$route_id)
+
+    # 'fare_rules' and 'trips' (route_id)
+
+    if (gtfsio::check_field_exists(gtfs, "fare_rules", "route_id")) {
+      gtfsio::assert_field_class(gtfs, "fare_rules", "route_id", "character")
+      gtfs$fare_rules <- gtfs$fare_rules[route_id %chin% relevant_routes]
+    }
+
+    if (gtfsio::check_field_exists(gtfs, "trips", "route_id")) {
+      gtfsio::assert_field_class(gtfs, "trips", "route_id", "character")
+      gtfs$trips <- gtfs$trips[route_id %chin% relevant_routes]
+
+      # 'trips' allows us to filter by 'trip_id', 'service_id' and 'shape_id'
+      relevant_services <- unique(gtfs$trips$service_id)
+      relevant_shapes <- unique(gtfs$trips$shape_id)
+      relevant_trips <- unique(gtfs$trips$trip_id)
+
+      # 'shapes' (shape_id)
+
+      if (gtfsio::check_field_exists(gtfs, "shapes", "shape_id")) {
+        gtfsio::assert_field_class(gtfs, "shapes", "shape_id", "character")
+        gtfs$shapes <- gtfs$shapes[shape_id %chin% relevant_shapes]
+      }
+
+      # 'calendar' and 'calendar_dates' (service_id)
+
+      if (gtfsio::check_field_exists(gtfs, "calendar", "service_id")) {
+        gtfsio::assert_field_class(gtfs, "calendar", "service_id", "character")
+        gtfs$calendar <- gtfs$calendar[service_id %chin% relevant_services]
+      }
+
+      if (gtfsio::check_field_exists(gtfs, "calendar_dates", "service_id")) {
+        gtfsio::assert_field_class(
+          gtfs,
+          "calendar_dates",
+          "service_id",
+          "character"
+        )
+        gtfs$calendar_dates <- gtfs$calendar_dates[
+          service_id %chin% relevant_services
+        ]
+      }
+
+      # 'stop_times' and 'frequencies' (trip_id)
+
+      if (gtfsio::check_field_exists(gtfs, "frequencies", "trip_id")) {
+        gtfsio::assert_field_class(gtfs, "frequencies", "trip_id", "character")
+        gtfs$frequencies <- gtfs$frequencies[trip_id %chin% relevant_trips]
+      }
+
+      if (gtfsio::check_field_exists(gtfs, "stop_times", "trip_id")) {
+        gtfsio::assert_field_class(gtfs, "stop_times", "trip_id", "character")
+        gtfs$stop_times <- gtfs$stop_times[trip_id %chin% relevant_trips]
+
+        # 'stop_times' allows us to filter by 'stop_id'. it's important to keep,
+        # however, not only the stops that appear on stop_times, but also their
+        # parent stops, that may not be listed on such file
+        # (get_parent_station() may raise a warning if a stop is present in
+        # 'stop_times' but not in 'stops', which will be suppressed for now)
+
+        relevant_stops <- unique(gtfs$stop_times$stop_id)
+
+        if (gtfsio::check_field_exists(gtfs, "stops", "parent_station")) {
+          suppressWarnings(
+            stops_with_parents <- get_parent_station(gtfs, relevant_stops)
+          )
+          relevant_stops <- stops_with_parents$stop_id
+        }
+
+        # 'stops', 'transfers' and 'pathways' (stop_id)
+
+        from_to_stop_id <- c("from_stop_id", "to_stop_id")
+
+        if (gtfsio::check_field_exists(gtfs, "transfers", from_to_stop_id)) {
+          gtfsio::assert_field_class(
+            gtfs,
+            "transfers",
+            from_to_stop_id,
+            rep("character", 2)
+          )
+          gtfs$transfers <- gtfs$transfers[
+            from_stop_id %chin% relevant_stops &
+              to_stop_id %chin% relevant_stops
+          ]
+        }
+
+        if (gtfsio::check_field_exists(gtfs, "pathways", from_to_stop_id)) {
+          gtfsio::assert_field_class(
+            gtfs,
+            "pathways",
+            from_to_stop_id,
+            rep("character", 2)
+          )
+          gtfs$pathways <- gtfs$pathways[
+            from_stop_id %chin% relevant_stops &
+              to_stop_id %chin% relevant_stops
+          ]
+        }
+
+        if (gtfsio::check_field_exists(gtfs, "stops", "stop_id")) {
+          gtfsio::assert_field_class(gtfs, "stops", "stop_id", "character")
+          gtfs$stops <- gtfs$stops[stop_id %chin% relevant_stops]
+
+          # 'stops' allows us to filter by 'level_id'
+          relevant_levels <- unique(gtfs$stops$level_id)
+
+          # 'levels' (level_id)
+
+          if (gtfsio::check_field_exists(gtfs, "levels", "level_id")) {
+            gtfsio::assert_field_class(gtfs, "levels", "level_id", "character")
+            gtfs$levels <- gtfs$levels[level_id %chin% relevant_levels]
+          }
+        }
+      }
+    }
+  }
+
+  return(gtfs)
+}
---
+++ tests/testthat/test-filter_by_agency_id.R
@@ -0,0 +1,169 @@
+context("Filter by agency_id")
+
+
+# setup -------------------------------------------------------------------
+
+
+ber_path <- system.file("extdata/ber_gtfs.zip", package = "gtfstools")
+ber_gtfs <- read_gtfs(ber_path)
+ber_agency <- "92"
+
+ggl_path <- system.file("extdata/ggl_gtfs.zip", package = "gtfstools")
+ggl_gtfs <- read_gtfs(ggl_path)
+ggl_agency <- "agency001"
+
+
+# tests -------------------------------------------------------------------
+
+
+test_that("raises error due to incorrect input types", {
+  expect_error(filter_by_agency_id(unclass(ber_gtfs), ber_agency))
+  expect_error(filter_by_agency_id(ber_gtfs, factor(ber_agency)))
+  expect_error(filter_by_agency_id(ber_gtfs, ber_agency, keep = "TRUE"))
+})
+
+test_that("results in a dt_gtfs object", {
+  # a dt_gtfs object is a list with "dt_gtfs" and "gtfs" classes
+  dt_gtfs_class <- c("dt_gtfs", "gtfs", "list")
+  smaller_gtfs <- filter_by_agency_id(ber_gtfs, ber_agency)
+  expect_s3_class(smaller_gtfs, dt_gtfs_class)
+  expect_type(smaller_gtfs, "list")
+
+  # all objects inside a dt_gtfs are data.tables
+  invisible(lapply(smaller_gtfs, expect_s3_class, "data.table"))
+})
+
+test_that("doesn't change given gtfs", {
+  # (except for some tables' indices)
+
+  original_gtfs <- read_gtfs(ber_path)
+  gtfs <- read_gtfs(ber_path)
+  expect_identical(original_gtfs, gtfs)
+
+  smaller_gtfs <- filter_by_agency_id(gtfs, ber_agency)
+  expect_false(identical(original_gtfs, gtfs))
+
+  data.table::setindex(gtfs$calendar, NULL)
+  data.table::setindex(gtfs$calendar_dates, NULL)
+  data.table::setindex(gtfs$shapes, NULL)
+  data.table::setindex(gtfs$stop_times, NULL)
+  data.table::setindex(gtfs$stops, NULL)
+  data.table::setindex(gtfs$trips, NULL)
+  expect_identical(original_gtfs, gtfs)
+})
+
+test_that("'agency_id' and 'keep' arguments work correctly", {
+  # adding a agency_id to routes and fare_attributes for the sake of coverage
+  ggl_gtfs$routes[, agency_id := "agency001"]
+  ggl_gtfs$fare_attributes[, agency_id := "agency001"]
+
+  smaller_keeping <- filter_by_agency_id(ggl_gtfs, ggl_agency)
+  expect_true(all(smaller_keeping$agency$agency_id %chin% ggl_agency))
+  expect_true(all(smaller_keeping$routes$agency_id %chin% ggl_agency))
+  expect_true(all(smaller_keeping$fare_attributes$agency_id %chin% ggl_agency))
+  expect_true(all(smaller_keeping$attributions$agency_id %chin% ggl_agency))
+
+  smaller_dropping <- filter_by_agency_id(ggl_gtfs, ggl_agency, keep = FALSE)
+  expect_true(!any(smaller_dropping$agency$agency_id %chin% ggl_agency))
+  expect_true(!any(smaller_dropping$routes$agency_id %chin% ggl_agency))
+  expect_true(
+    !any(smaller_dropping$fare_attributes$agency_id %chin% ggl_agency)
+  )
+  expect_true(!any(smaller_dropping$attributions$agency_id %chin% ggl_agency))
+})
+
+test_that("the function filters berlin's gtfs correctly", {
+  smaller_ber <- filter_by_agency_id(ber_gtfs, ber_agency)
+
+  # agency
+  expect_true(nrow(smaller_ber$agency) == 1)
+  expect_true(smaller_ber$agency$agency_id == ber_agency)
+
+  # routes
+  expect_true(all(smaller_ber$routes$agency_id %chin% ber_agency))
+  relevant_routes <- unique(smaller_ber$routes$route_id)
+
+  # fare_rules
+  expect_true(all(smaller_ber$fare_rules$route_id %chin% relevant_routes))
+
+  # trips
+  expect_true(all(smaller_ber$trips$route_id %chin% relevant_routes))
+  relevant_shapes <- unique(smaller_ber$trips$shape_id)
+  relevant_services <- unique(smaller_ber$trips$service_id)
+  relevant_trips <- unique(smaller_ber$trips$trip_id)
+
+  # shapes
+  expect_true(all(smaller_ber$shapes$shape_id %chin% relevant_shapes))
+
+  # calendar and calendar_dates
+  expect_true(all(smaller_ber$calendar$service_id %chin% relevant_services))
+  expect_true(
+    all(smaller_ber$calendar_dates$service_id %chin% relevant_services)
+  )
+
+  # stop_times
+  expect_true(all(smaller_ber$stop_times$trip_id %chin% relevant_trips))
+
+  # stops
+  relevant_stops <- unique(
+    ber_gtfs$stop_times[trip_id %chin% relevant_trips]$stop_id
+  )
+  relevant_stops <- get_parent_station(ber_gtfs, relevant_stops)$stop_id
+  expect_true(all(smaller_ber$stops$stop_id %chin% relevant_stops))
+})
+
+test_that("the function filters google's gtfs correctly", {
+  # adding a agency_id to routes and fare_attributes for the sake of coverage
+  ggl_gtfs$routes[, agency_id := "agency001"]
+  ggl_gtfs$fare_attributes[, agency_id := "agency001"]
+  smaller_ggl <- filter_by_agency_id(ggl_gtfs, ggl_agency)
+
+  # agency
+  expect_true(smaller_ggl$agency$agency_id == ggl_agency)
+
+  # attributions
+  expect_true(all(smaller_ggl$attributions$agency_id == ggl_agency))
+
+  # fare_attributes
+  expect_true(all(smaller_ggl$fare_attributes$agency_id == ggl_agency))
+
+  # routes
+  expect_true(all(smaller_ggl$routes$agency_id %chin% ggl_agency))
+  relevant_routes <- unique(smaller_ggl$routes$route_id)
+
+  # fare_rules - none of the route_ids listed are in routes
+  expect_true(nrow(smaller_ggl$fare_rules) == 0)
+
+  # trips
+  expect_true(all(smaller_ggl$trips$route_id %chin% relevant_routes))
+  relevant_shapes <- unique(smaller_ggl$trips$shape_id)
+  relevant_services <- unique(smaller_ggl$trips$service_id)
+  relevant_trips <- unique(smaller_ggl$trips$trip_id)
+
+  # shapes - none of the shape_ids listed are in trips
+  expect_true(nrow(smaller_ggl$shapes) == 0)
+
+  # calendar and calendar_dates
+  expect_true(all(smaller_ggl$calendar$service_id %chin% relevant_services))
+  expect_true(
+    all(smaller_ggl$calendar_dates$service_id %chin% relevant_services)
+  )
+
+  # stop_times
+  expect_true(all(smaller_ggl$stop_times$trip_id %chin% relevant_trips))
+
+  # stops - none of the stop_ids listed are in stop_times
+  expect_true(nrow(smaller_ggl$stops) == 0)
+
+  # transfers - no rows either
+  expect_true(nrow(smaller_ggl$transfers) == 0)
+
+  # pathways - no rows either
+  expect_true(nrow(smaller_ggl$pathways) == 0)
+
+  # levels - no rows either
+  expect_true(nrow(smaller_ggl$levels) == 0)
+
+  # frequencies
+  expect_true(all(smaller_ggl$frequencies$trip_id %chin% relevant_trips))
+})
