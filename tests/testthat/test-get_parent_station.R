6decda8d1503fb849b5d90d5039d2f96d714d614
Daniel Herszenhut <dhersz@gmail.com>
2022-05-20 11:19:06 -0300

    changed get_parent_station() to allow stop_id = NULL

--- tests/testthat/test-get_parent_station.R
+++ tests/testthat/test-get_parent_station.R
@@ -1,29 +1,22 @@
 data_path <- system.file("extdata/ggl_gtfs.zip", package = "gtfstools")
 gtfs <- read_gtfs(data_path)
+stop_id <- "N1"
 
-
-# tests -------------------------------------------------------------------
-
+tester <- function(gtfs = get("gtfs", envir = parent.frame()), stop_id = NULL) {
+  get_parent_station(gtfs, stop_id)
+}
 
 test_that("raises errors due to incorrect input types/value", {
-  no_class_gtfs <- unclass(gtfs)
-  expect_error(get_parent_station(no_class_gtfs))
-  expect_error(get_parent_station(gtfs, as.factor("N1")))
-  expect_error(get_parent_station(gtfs, NA))
+  expect_error(tester(unclass(gtfs)))
+  expect_error(tester(gtfs, as.factor("N1")))
+  expect_error(tester(gtfs, NA))
 })
 
 test_that("raises errors if reqrd fields do not exist or have the right type", {
-
-  # create gtfs without 'stop_times'
-
   no_stops_gtfs <- copy_gtfs_without_file(gtfs, "stops")
-
-  # create gtfs without relevant fields
 
   no_stp_id_gtfs <- copy_gtfs_without_field(gtfs, "stops", "stop_id")
   no_prt_st_gtfs <- copy_gtfs_without_field(gtfs, "stops", "parent_station")
-
-  # create gtfs with fields of wrong class
 
   wrong_stp_id_gtfs <- copy_gtfs_diff_field_class(
     gtfs,
@@ -38,54 +31,58 @@ test_that("raises errors if reqrd fields do not exist or have the right type", {
     "factor"
   )
 
-  expect_error(get_parent_station(no_stops_gtfs, "N1"))
-  expect_error(get_parent_station(no_stp_id_gtfs, "N1"))
-  expect_error(get_parent_station(no_prt_st_gtfs, "N1"))
-  expect_error(get_parent_station(wrong_stp_id_gtfs, "N1"))
-  expect_error(get_parent_station(wrong_prt_st_gtfs, "N1"))
-
+  expect_error(tester(no_stops_gtfs), class = "missing_required_file")
+  expect_error(tester(no_stp_id_gtfs), class = "missing_required_field")
+  expect_error(tester(no_prt_st_gtfs), class = "missing_required_field")
+  expect_error(tester(wrong_stp_id_gtfs), class = "wrong_class_field")
+  expect_error(tester(wrong_prt_st_gtfs), class = "wrong_class_field")
 })
 
 test_that("raises warnings if a non_existent stop_id is passed", {
-  expect_warning(get_parent_station(gtfs, c("N1", "ola")))
-  expect_warning(get_parent_station(gtfs, "ola"))
+  expect_warning(tester(stop_id = c("N1", "ola")))
+  expect_warning(tester(stop_id = "ola"))
 })
 
 test_that("outputs a data.table with adequate columns' classes", {
-
-  parents <- get_parent_station(gtfs, "N1")
-
-  # result is a data.table
-
+  parents <- tester()
   expect_s3_class(parents, "data.table")
-
-  # columns' types
-
-  expect_vector(parents$stop_id, character(0))
-  expect_vector(parents$parent_station, character(0))
+  expect_type(parents$stop_id, "character")
+  expect_type(parents$parent_station, "character")
 
   # should work even if no given 'stop_id's are present in 'stops'
 
-  expect_warning(parents <- get_parent_station(gtfs, "ola"))
-
+  expect_warning(parents <- tester(stop_id = "ola"))
   expect_s3_class(parents, "data.table")
+  expect_type(parents$stop_id, "character")
+  expect_type(parents$parent_station, "character")
 
-  expect_vector(parents$stop_id, character(0))
-  expect_vector(parents$parent_station, character(0))
+  # and when stop_id = character(0)
 
+  parents <- tester(stop_id = character(0))
+  expect_s3_class(parents, "data.table")
+  expect_type(parents$stop_id, "character")
+  expect_type(parents$parent_station, "character")
+})
+
+test_that("returns parents of correct stops", {
+  parents <- tester()
+  expect_true(all(gtfs$stops$stop_id %chin% parents$stop_id))
+
+  parents <- tester(stop_id = stop_id)
+  expect_identical(parents$stop_id, c("N1", "F12"))
 })
 
 test_that("returns parents correctly", {
   # when none of the passed stops have parents, all parents will be "" and
   # the data.table will have as many as rows as the number of stop_ids passed
-  parents <- get_parent_station(gtfs, "F12")
+  parents <- tester(stop_id = "F12")
   expect_identical(
     parents,
     data.table::data.table(stop_id = "F12", parent_station = "")
   )
 
   # if they have parents, their parents' parents will be searched recursively
-  parents <- get_parent_station(gtfs, "B1")
+  parents <- tester(stop_id = "B1")
   expect_identical(
     parents,
     data.table::data.table(
@@ -95,7 +92,7 @@ test_that("returns parents correctly", {
   )
 
   # and non-existent ids are not included in the result
-  expect_warning(parents <- get_parent_station(gtfs, "ola"))
+  expect_warning(parents <- tester(stop_id = "ola"))
   expect_identical(
     parents,
     data.table::data.table(
@@ -110,7 +107,7 @@ test_that("doesn't change original gtfs", {
   gtfs <- read_gtfs(data_path)
   expect_identical(original_gtfs, gtfs)
 
-  parents <- get_parent_station(gtfs, "B1")
+  parents <- tester(stop_id = "B1")
   expect_identical(original_gtfs, gtfs)
 })
 
@@ -122,7 +119,7 @@ test_that("unlisted parent_stations do not introduce NAs", {
 
   smaller_ber <- filter_by_shape_id(ber_gtfs, ber_shapes)
 
-  parents <- get_parent_station(ber_gtfs, smaller_ber$stop_times$stop_id)
+  parents <- tester(ber_gtfs, smaller_ber$stop_times$stop_id)
   expect_false(any(is.na(parents$stop_id)))
   expect_false(any(is.na(parents$parent_station)))
 })
