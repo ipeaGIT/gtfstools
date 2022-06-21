2625d4cd5308b1e41d94510ae636d9183e3472de
Daniel Herszenhut <dhersz@gmail.com>
2022-06-21 16:53:20 -0300

    allow gtfs objects, paths and urls in validate_gtfs()

--- R/validate_gtfs.R
+++ R/validate_gtfs.R
@@ -11,6 +11,8 @@
 #'   and in which the results will be saved to.
 #' @param validator_path The path to the GTFS validator, previously downloaded
 #'   with [download_validator()].
+#' @param html_preview Whether to show HTML report in a viewer, such as RStudio
+#'   or a browser. Defaults to `TRUE` (only works on interactive sessions).
 #'
 #' @return Invisibly returns the normalized path to the directory where the
 #'   validation results were saved to.
@@ -18,19 +20,64 @@
 #' @family validation
 #'
 #' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
-#' gtfs_path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
+#' data_path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
 #' output_path <- tempfile("validation_result")
 #' validator_path <- download_validator(tempdir())
+#' gtfs <- read_gtfs(data_path)
 #'
-#' validate_gtfs(gtfs_path, output_path, validator_path)
+#' validate_gtfs(gtfs, output_path, validator_path)
+#' list.files(output_path)
+#'
+#' # works with feeds saved to disk
+#' new_output_path <- tempfile("new_validation_result")
+#' validate_gtfs(data_path, new_output_path, validator_path)
+#' list.files(new_output_path)
+#'
+#' # and with feeds pointed by an url
+#' newer_output_path <- tempfile("newer_validation_result")
+#' gtfs_url <- "https://github.com/ipeaGIT/gtfstools/raw/master/inst/extdata/spo_gtfs.zip"
+#' validate_gtfs(gtfs_url, newer_output_path, validator_path)
+#' list.files(newer_output_path)
+#'
 #' @export
-validate_gtfs <- function(gtfs, output_path, validator_path) {
+validate_gtfs <- function(gtfs,
+                          output_path,
+                          validator_path,
+                          html_preview = TRUE) {
   assert_java_version()
+  checkmate::assert_path_for_output(output_path)
+  checkmate::assert_file_exists(validator_path)
+  checkmate::assert_logical(html_preview, any.missing = FALSE, len = 1)
 
-  call_output <- processx::run(
-    "java",
-    c("-jar", validator_path, "-i", gtfs, "-o", output_path)
-  )
+  if (!inherits(gtfs, "dt_gtfs")) {
+    if (!checkmate::test_string(gtfs)) {
+      stop(
+        "Assertion on 'gtfs' failed: Must either be a GTFS object (with ",
+        "dt_gtfs class), a path to a local GTFS feed or an URL to a feed."
+      )
+    }
+    is_url <- grepl("^http[s]?\\:\\/\\/\\.*", gtfs)
+    if (!(file.exists(gtfs) || is_url)) {
+      stop(
+        "Assertion on 'gtfs' failed: Must either be a GTFS object (with ",
+        "dt_gtfs class), a path to a local GTFS feed or an URL to a feed."
+      )
+    }
+  }
+
+  command_flags <- c("-jar", validator_path, "-o", output_path)
+
+  if (inherits(gtfs, "dt_gtfs")) {
+    gtfs_path <- tempfile("gtfs", fileext = ".zip")
+    write_gtfs(gtfs, gtfs_path)
+    command_flags <- c(command_flags, "-i", gtfs_path)
+  } else if (!is_url) {
+    command_flags <- c(command_flags, "-i", gtfs)
+  } else {
+    command_flags <- c(command_flags, "-u", gtfs)
+  }
+
+  call_output <- processx::run("java", command_flags)
 
   if (call_output$stdout != "") {
     writeLines(
@@ -44,6 +91,11 @@ validate_gtfs <- function(gtfs, output_path, validator_path) {
       call_output$stderr,
       con = file.path(output_path, "validation_stderr.txt")
     )
+  }
+
+  if (interactive() && html_preview) {
+    html_path <- file.path(output_path, "report.html")
+    utils::browseURL(html_path)
   }
 
   return(invisible(normalizePath(output_path)))
