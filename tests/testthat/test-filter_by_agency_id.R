71487d4587761c0f1dd300a5ef1f8449bdc6d74d
Daniel Herszenhut <dhersz@gmail.com>
2023-11-01 12:58:35 -0300

    simplified filter_by_agency_id() tests a bit

--- tests/testthat/test-filter_by_agency_id.R
+++ tests/testthat/test-filter_by_agency_id.R
@@ -12,23 +12,22 @@ tester <- function(gtfs = ber_gtfs, agency_id = ber_agency, keep = TRUE) {
 
 # tests -------------------------------------------------------------------
 
-
 test_that("raises error due to incorrect input types", {
-  expect_error(filter_by_agency_id(unclass(ber_gtfs), ber_agency))
-  expect_error(filter_by_agency_id(ber_gtfs, factor(ber_agency)))
-  expect_error(filter_by_agency_id(ber_gtfs, NA))
-  expect_error(filter_by_agency_id(ber_gtfs, ber_agency, keep = "TRUE"))
-  expect_error(filter_by_agency_id(ber_gtfs, ber_agency, keep = NA))
+  expect_error(tester(unclass(ber_gtfs)))
+
+  expect_error(tester(agency_id = factor(ber_agency)))
+  expect_error(tester(agency_id = NA))
+
+  expect_error(tester(keep = "TRUE"))
+  expect_error(tester(keep = NA))
 })
 
 test_that("results in a dt_gtfs object", {
-  # a dt_gtfs object is a list with "dt_gtfs" and "gtfs" classes
   dt_gtfs_class <- c("dt_gtfs", "gtfs", "list")
-  smaller_gtfs <- filter_by_agency_id(ber_gtfs, ber_agency)
+
+  smaller_gtfs <- tester()
   expect_s3_class(smaller_gtfs, dt_gtfs_class)
   expect_type(smaller_gtfs, "list")
-
-  # all objects inside a dt_gtfs are data.tables
   invisible(lapply(smaller_gtfs, expect_s3_class, "data.table"))
 })
 
@@ -39,9 +38,8 @@ test_that("doesn't change given gtfs", {
   gtfs <- read_gtfs(ber_path)
   expect_identical(original_gtfs, gtfs)
 
-  smaller_gtfs <- filter_by_agency_id(gtfs, ber_agency)
+  smaller_gtfs <- tester(gtfs, ber_agency)
   expect_false(identical(original_gtfs, gtfs))
-
   data.table::setindex(gtfs$calendar, NULL)
   data.table::setindex(gtfs$calendar_dates, NULL)
   data.table::setindex(gtfs$shapes, NULL)
@@ -56,13 +54,13 @@ test_that("'agency_id' and 'keep' arguments work correctly", {
   ggl_gtfs$routes[, agency_id := "agency001"]
   ggl_gtfs$fare_attributes[, agency_id := "agency001"]
 
-  smaller_keeping <- filter_by_agency_id(ggl_gtfs, ggl_agency)
+  smaller_keeping <- tester(ggl_gtfs, ggl_agency)
   expect_true(all(smaller_keeping$agency$agency_id %chin% ggl_agency))
   expect_true(all(smaller_keeping$routes$agency_id %chin% ggl_agency))
   expect_true(all(smaller_keeping$fare_attributes$agency_id %chin% ggl_agency))
   expect_true(all(smaller_keeping$attributions$agency_id %chin% ggl_agency))
 
-  smaller_dropping <- filter_by_agency_id(ggl_gtfs, ggl_agency, keep = FALSE)
+  smaller_dropping <- tester(ggl_gtfs, ggl_agency, keep = FALSE)
   expect_true(!any(smaller_dropping$agency$agency_id %chin% ggl_agency))
   expect_true(!any(smaller_dropping$routes$agency_id %chin% ggl_agency))
   expect_true(
@@ -72,7 +70,7 @@ test_that("'agency_id' and 'keep' arguments work correctly", {
 })
 
 test_that("the function filters berlin's gtfs correctly", {
-  smaller_ber <- filter_by_agency_id(ber_gtfs, ber_agency)
+  smaller_ber <- tester()
 
   # agency
   expect_true(nrow(smaller_ber$agency) == 1)
@@ -115,7 +113,7 @@ test_that("the function filters google's gtfs correctly", {
   # adding a agency_id to routes and fare_attributes for the sake of coverage
   ggl_gtfs$routes[, agency_id := "agency001"]
   ggl_gtfs$fare_attributes[, agency_id := "agency001"]
-  smaller_ggl <- filter_by_agency_id(ggl_gtfs, ggl_agency)
+  smaller_ggl <- tester(ggl_gtfs, ggl_agency)
 
   # agency
   expect_true(smaller_ggl$agency$agency_id == ggl_agency)
@@ -169,15 +167,15 @@ test_that("the function filters google's gtfs correctly", {
 
 test_that("behaves correctly when agency_id = character(0)", {
   # if keep = TRUE, gtfs should be empty
-  empty <- filter_by_agency_id(ber_gtfs, character(0))
+  empty <- tester(agency_id = character(0))
   n_rows <- vapply(empty, nrow, FUN.VALUE = integer(1))
   expect_true(all(n_rows == 0))
 
   # if keep = FALSE, gtfs should remain unchanged
   # this is actually not true because the calendar, calendar_dates and agency
-  # tables contain ids not listed in the routes and trips tables, which and up
+  # tables contain ids not listed in the routes and trips tables, which end up
   # removed anyway (I like this behaviour, so not considering a bug)
-  full <- filter_by_agency_id(ber_gtfs, character(0), keep = FALSE)
+  full <- tester(ber_gtfs, character(0), keep = FALSE)
   modified_ber <- read_gtfs(ber_path)
   modified_ber$calendar <- modified_ber$calendar[
     service_id %in% modified_ber$trips$service_id
