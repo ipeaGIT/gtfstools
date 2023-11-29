785290a03a6269e8cf058bc0ca09c3b8fc60c846
Daniel Herszenhut <dhersz@gmail.com>
2023-11-29 11:31:53 -0300

    included full_trips parameter to filter_by_stop_id() to deprecate it

--- R/filter_by_stop_id.R
+++ R/filter_by_stop_id.R
@@ -1,15 +1,20 @@
 #' Filter GTFS object by `stop_id`
 #'
 #' Filters a GTFS object by `stop_id`s, keeping (or dropping) relevant entries
-#' in each file. In order to keep the integrity of trips as described in the
-#' `stop_times` table, the `stop_id`s are actually used to filter `trip_id`s,
-#' which are then used to filter the rest of the GTFS object.
+#' in each file.
 #'
 #' @template gtfs
 #' @param stop_id A character vector. The `stop_id`s used to filter the data.
 #' @param keep A logical. Whether the entries related to the `trip_id`s that
 #'   passes through the specified `stop_id`s should be kept or dropped (defaults
 #'   to `TRUE`, which keeps the entries).
+#' @param full_trips A logical. Whether to keep all stops that compose trips
+#'   that pass through the stops specified in `stop_id`. Defaults to `TRUE`, in
+#'   order to preserve the behavior of the function in versions 1.2.0 and below.
+#'   Please note that when `TRUE`, the resultant filtered feed may contain more
+#'   stops than the ones specified in `stop_id` to preserve the integrity of the
+#'   trips. IMPORTANT: this parameter will cease to exist from version 2.0.0
+#'   onward.
 #'
 #' @return The GTFS object passed to the `gtfs` parameter, after the filtering
 #' process.
@@ -32,22 +37,23 @@
 #' object.size(smaller_gtfs)
 #'
 #' @export
-filter_by_stop_id <- function(gtfs, stop_id, keep = TRUE) {
+filter_by_stop_id <- function(gtfs, stop_id, keep = TRUE, full_trips = TRUE) {
   gtfs <- assert_and_assign_gtfs_object(gtfs)
   checkmate::assert_character(stop_id, any.missing = FALSE)
   checkmate::assert_logical(keep, len = 1, any.missing = FALSE)
+  checkmate::assert_logical(full_trips, len = 1, any.missing = FALSE)
 
-  env <- environment()
+  if (full_trips) {
+    env <- environment()
 
-  if (gtfsio::check_field_exists(gtfs, "stop_times", "stop_id")) {
+    if (gtfsio::check_field_exists(gtfs, "stop_times", "stop_id")) {
+      gtfsio::assert_field_class(gtfs, "stop_times", "stop_id", "character")
+      relevant_trips <- unique(
+        gtfs$stop_times[stop_id %chin% get("stop_id", envir = env)]$trip_id
+      )
 
-    gtfsio::assert_field_class(gtfs, "stop_times", "stop_id", "character")
-    relevant_trips <- unique(
-      gtfs$stop_times[stop_id %chin% get("stop_id", envir = env)]$trip_id
-    )
-
-    gtfs <- filter_by_trip_id(gtfs, relevant_trips, keep)
-
+      gtfs <- filter_by_trip_id(gtfs, relevant_trips, keep)
+    }
   }
 
   return(gtfs)
--- tests/testthat/test-filter_by_stop_id.R
+++ tests/testthat/test-filter_by_stop_id.R
@@ -1,57 +1,62 @@
-path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
-gtfs <- read_gtfs(path)
-stop_ids <- c("18848", "940004157")
-env <- environment()
+spo_path <- system.file("extdata/spo_gtfs.zip", package = "gtfstools")
+spo_gtfs <- read_gtfs(spo_path)
+relevant_stops <- c("18848", "940004157")
+
+tester <- function(gtfs = spo_gtfs,
+                   stop_id = relevant_stops,
+                   keep = TRUE,
+                   full_trips = TRUE) {
+  filter_by_stop_id(gtfs, stop_id, keep, full_trips)
+}
 
 # tests -------------------------------------------------------------------
-# the large majority of tests are already conducted inside filter_by_trip_id(),
-# which filter_by_stop_id() calls. only a few sanity checks are included here
 
+# full_trips = TRUE
 
 test_that("raises error due to incorrect input types", {
-  expect_error(filter_by_stop_id(unclass(gtfs), stop_ids))
-  expect_error(filter_by_stop_id(gtfs, factor(stop_ids)))
-  expect_error(filter_by_stop_id(gtfs, NA))
-  expect_error(filter_by_stop_id(gtfs, stop_ids, keep = "TRUE"))
-  expect_error(filter_by_stop_id(gtfs, stop_ids, NA))
+  expect_error(tester(unclass(spo_gtfs)))
+
+  expect_error(tester(stop_id = factor(relevant_stops)))
+  expect_error(tester(stop_id = NA))
+
+  expect_error(tester(keep = "TRUE"))
+  expect_error(tester(keep = c(TRUE, TRUE)))
+  expect_error(tester(keep = NA))
+
+  expect_error(tester(full_trips = "TRUE"))
+  expect_error(tester(full_trips = c(TRUE, TRUE)))
+  expect_error(tester(full_trips = NA))
 })
 
 test_that("results in a dt_gtfs object", {
-  # a dt_gtfs object is a list with "dt_gtfs" and "gtfs" classes
   dt_gtfs_class <- c("dt_gtfs", "gtfs", "list")
-  smaller_gtfs <- filter_by_stop_id(gtfs, stop_ids)
+
+  smaller_gtfs <- tester()
   expect_s3_class(smaller_gtfs, dt_gtfs_class)
   expect_type(smaller_gtfs, "list")
-
-  # all objects inside a dt_gtfs are data.tables
   invisible(lapply(smaller_gtfs, expect_s3_class, "data.table"))
 })
 
 test_that("doesn't change given gtfs", {
   # (except for some tables' indices)
 
-  original_gtfs <- read_gtfs(path)
-  gtfs <- read_gtfs(path)
+  original_gtfs <- read_gtfs(spo_path)
+  gtfs <- read_gtfs(spo_path)
   expect_identical(original_gtfs, gtfs)
 
-  smaller_gtfs <- filter_by_stop_id(gtfs, stop_ids)
+  smaller_gtfs <- tester(gtfs)
   expect_false(identical(original_gtfs, gtfs))
-
-  data.table::setindex(gtfs$agency, NULL)
-  data.table::setindex(gtfs$calendar, NULL)
-  data.table::setindex(gtfs$frequencies, NULL)
-  data.table::setindex(gtfs$routes, NULL)
-  data.table::setindex(gtfs$shapes, NULL)
-  data.table::setindex(gtfs$stop_times, NULL)
   data.table::setindex(gtfs$stops, NULL)
+  data.table::setindex(gtfs$stop_times, NULL)
   expect_identical(original_gtfs, gtfs)
 })
 
 test_that("'stop_id' and 'keep' arguments work correctly", {
   relevant_trips <- gtfs$stop_times[stop_id %chin% stop_ids]$trip_id
-  smaller_gtfs_keeping <- filter_by_stop_id(gtfs, stop_ids)
+
+  smaller_gtfs_keeping <- tester()
   expect_true(all(smaller_gtfs_keeping$trips$trip_id %in% relevant_trips))
 
-  smaller_gtfs_not_keeping <- filter_by_stop_id(gtfs, stop_ids, keep = FALSE)
+  smaller_gtfs_not_keeping <- tester(keep = FALSE)
   expect_true(!any(smaller_gtfs_not_keeping$trips$trip_id %in% relevant_trips))
 })
